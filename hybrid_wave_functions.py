64591b5ed8c7d10425dc4d3ae4b7eda68c19f8f8
JuanBohorquez3 <jbohorquez@wisc.edu>
2020-11-10 18:46:31 -0600

    Refactor and document

--- hybrid_wave_functions.py
+++ hybrid_wave_functions.py
@@ -5,6 +5,7 @@ from numpy import *  # Totally redundant but helps IDE understand our scope
 HSDIO = experiment.LabView.HSDIO.add_transition
 AO = experiment.LabView.AnalogOutput.add_transition
 label = experiment.functional_waveforms_graph.label
+add_repeat = experiment.LabView.HSDIO.add_repeat
 
 # Instrument Triggers ----------------------------------------------------------
 NIScope_Trig = lambda t, state: HSDIO(t, 2, state)  #Not Set!
@@ -208,10 +209,10 @@ def ramp(ti, duration, vi, vf, channel):
     """
     Ramp AO channel from vi to vf in duration
     Args:
-        ti: start time for the ramp
-        duration: length of time over which to ramp
-        vi: initial voltage
-        vf: final voltage
+        ti: start time (ms)
+        duration: length of time over which to ramp (ms), function is precise to the us
+        vi : initial voltage(s)
+        vf: final voltage(s)
         channel: int or callable, AO channel to be ramped. If an int the AO function is called on
             for this channel, if it's a callable it should an AO function which can be set to a
             float value at a time t.
@@ -225,26 +226,163 @@ def ramp(ti, duration, vi, vf, channel):
 
     ao_increment = 0.001  # Shortest allowed time between changes to AO waveform
     tf = ti + duration
-    times = arange(ti, tf, ao_increment)
+    times = arange(ti, tf+ao_increment, ao_increment)
     voltages = linspace(vi, vf, len(times))
     for t, v in zip(times, voltages):
         func(t, v)
     return tf
 
 
-# Device specific functions
-def trigNIDAQ(t):
+def biasAORamp(t, duration, start_v, end_v, steps=None):
+    """
+    Ramps shim coils from start condition to end condition
+    Args:
+        t (float): Start time (ms)
+        duration (float): duration of ramp (ms)
+        start_v (iterable): length 3 iterable of floats, initial voltages for shims to take (X,Y,Z)
+        end_v (iterable): length 3 iterable of floats, final voltages for shims to take (X,Y,Z)
+        steps (int): number of steps over which to complete the ramp
+
+    Returns:
+        t (float): End time (ms)
+
+    """
+    '''
+    # Old implementation
+    ar = array([[linspace(start_v[0], end_v[0], steps)[i],
+                 linspace(start_v[1], end_v[1], steps)[i],
+                 linspace(start_v[2], end_v[2], steps)[i]] for i in arange(steps)])
+    for i in arange(steps):
+        biasAO(t, ar[i])
+        t += duration / steps
+    biasAO(t, end_v)
+    return t
+    '''
+    ti = t
+    ao_increment = 0.001
+    tf = ti+duration
+    if steps is None:
+        times = append(arange(t, tf, ao_increment), tf)
+    else:
+        times = linspace(t, tf, steps)
+    voltages = linspace(start_v, end_v, len(times))
+
+    for t, v in zip(times, voltages):
+        biasAO(t, v)
+    return tf
+
+
+# Device specific functions ------------------------------------------------------------------------
+
+# pulsing triggers
+def trigger_nidaq(t):
     t = pulse(t, 1, NIDAQ_Trig)
     return t
 
 
+def trigger_ni_scope(t):
+    t = pulse(t, 1, NIScope_Trig)
+    return t
+
+
+def trigger_z_stage(t):
+    t = pulse(t, 1, Zstage_Trig)
+    return t
+
+
+# MOT Coil controls
 def MOTAOVolt(current):
+    """
+    Function that returns the necessary Analog Out voltage needed to provide the desired MOT coil
+        current
+    Args:
+        current (float): the value of the MOT coil current desired
+
+    Returns:
+        AO_voltage (float): the voltage to be output by the Analog Output device needed to provide
+            an output current of 'current' to the coils
+    """
     return (current - 0.24) / 8.100
 
 
 def switchcoils(t, state):
+    """
+    Switches the MOT coils on or off making use of both the HSDIO function and the AO function.
+    Args:
+        t (float): the time to start the switching the coils on or off
+        state (bool): Desired end state of the coils
+    Returns:
+        t (float): time after coils are switched. Currently equal to start time t
+    """
     MOT_coils_switch(t, state)
     MOT_coilsAO(t, MOTAOVolt(MOT_Coil_Current) * state)
+    return t
+
+
+def close_shutters(t, delay=True, closeXZShutter=0, closeYShutter=0, closeY2Shutter=0,
+                   closeXShutter=0):
+    """
+    close switchyard shutters
+    Args:
+        t (float): Start time (ms)
+        delay (bool): if true, return time is time when all shutters have reached their final state
+        closeXZShutter (bool): if true, XZ shutter is closed
+        closeYShutter (bool): if true, Y shutter is closed
+        closeY2Shutter (bool): if true, Y2 shutter is closed
+        closeXShutter (bool): if true, X shutter is closed
+
+    Returns:
+        t (float): End time. = t + max(shutter_delays)*delay
+    """
+    # TODO : Refactor this
+    if not delay:
+        delay_XZ_off = 0
+        delay_XZ_on = 0
+        delay_y_off = 0
+        delay_y_on = 0
+        delay_y2_on = 0
+        delay_y2_off = 0
+        delay_x_on = 0
+        delay_x_off = 0
+
+    else:
+        delay_XZ_off = XZ_Shutter_delay_off
+        delay_XZ_on = XZ_Shutter_delay_on
+        delay_y_on = Y1_Shutter_delay_on
+        delay_y_off = Y1_Shutter_delay_off
+        delay_y2_on = Y2_Shutter_delay_on
+        delay_y2_off = Y2_Shutter_delay_off
+        delay_x_on = X_Shutter_delay_on
+        delay_x_off = X_Shutter_delay_off
+
+    dly = 0
+    if closeXZShutter:
+        XZ_Only_Shutter_switch(t - delay_XZ_off, False)
+        dly = max(dly, delay_XZ_off)
+    else:
+        XZ_Only_Shutter_switch(t - delay_XZ_on, True)
+        dly = max(dly, delay_XZ_on)
+    if closeYShutter:
+        Y_Only_Shutter_switch(t - delay_y_off, False)
+        dly = max(dly, delay_y_off)
+    else:
+        Y_Only_Shutter_switch(t - delay_y_on, True)
+        dly = max(dly, delay_y_on)
+    if closeY2Shutter:
+        Y2_Only_Shutter_switch(t - delay_y2_off, False)
+        dly = max(dly, delay_y2_off)
+    else:
+        Y2_Only_Shutter_switch(t - delay_y2_on, True)
+        dly = max(dly, delay_y2_on)
+    if closeXShutter:
+        X_Only_Shutter_switch(t - delay_x_off, False)
+        dly = max(dly, delay_x_off)
+    else:
+        X_Only_Shutter_switch(t - delay_x_on, True)
+        dly = max(dly, delay_x_on)
+
+    return t + dly
+
 
 # Shutter timing calibrations
 RP_Shutter_delay_off = 3.1
